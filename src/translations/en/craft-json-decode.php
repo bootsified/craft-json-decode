ecf6868abd1843e8a43e8e06d6be380788ceb21b
Boots Highland <boots@simplygoodwork.com>
2018-06-28 11:59:25 -0500

    ALL DA FILEZZZ!!!!!!!!!

---
+++ src/translations/en/craft-json-decode.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * craft-json-decode plugin for Craft CMS 3.x
+ *
+ * A SUPER simple plugin that decodes JSON data for use in Craft CMS v3 templates.
+ *
+ * @link      http://boots.media
+ * @copyright Copyright (c) 2018 Boots Highland
+ */
+
+/**
+ * craft-json-decode en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-json-decode', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Boots Highland
+ * @package   Craftjsondecode
+ * @since     1.0.0
+ */
+return [
+    'craft-json-decode plugin loaded' => 'craft-json-decode plugin loaded',
+];
